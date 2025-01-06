6aa9aefff4e22e410caa4101459f74630693dbb0
Raul Escobedo <rjescobedo@gmail.com>
2025-01-06 07:39:42 -0600

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 // Write your code here!
+const userInfo = {
+    username: 'flatUser123',
+    description: 'Hi, welcome to my webpage! Just a new user of this website!',
+    theme: 'blue'
+}
 
+function displayUser(userInfo) {
+   const header = document.querySelector('#main');
+   header.textContent = `${userInfo.username}'s Personal Website!`
+   header.className = userInfo.theme;
+
+   const description = document.createElement('p');
+   const body = document.querySelector('body');
+
+   description.textContent = userInfo.description;
+   description.className = userInfo.theme;
+
+   body.append(description);
+
+}
+
+displayUser(userInfo);
